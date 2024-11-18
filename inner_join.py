d7d249f86f67a3fa78730367d976bef3cdbd59d7
surajiiit <132198515+surajiiit@users.noreply.github.com>
2024-11-18 10:30:50 +0530

    Update inner_join.py
    Added Data from 29-12-2023 To 14-11-2024

--- inner_join.py
+++ inner_join.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
 # Load the CSV files into DataFrames
-gold_bees = pd.read_csv('gold_bees.csv')
+gold_bees = pd.read_csv('data_set.csv')
 sensex = pd.read_csv('sensex.csv')
 nifty50 = pd.read_csv('nifty50.csv')
-usd_eur = pd.read_csv('usd_eur.csv')
+eur_usd = pd.read_csv('eur_usd.csv')
 crude_oil = pd.read_csv('crude_oil.csv')
 
 # Perform INNER JOIN based on the 'date' column
 merged_data = gold_bees.merge(sensex, on='Date', how='inner') \
     .merge(nifty50, on='Date', how='inner') \
-    .merge(usd_eur, on='Date', how='inner') \
+    .merge(eur_usd, on='Date', how='inner') \
     .merge(crude_oil, on='Date', how='inner')
 
 # Display the result
